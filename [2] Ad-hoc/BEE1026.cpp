fe98c41408073011bae681fb925afa3145fda021
Drianold <56211609+Drianold@users.noreply.github.com>
2021-11-13 08:19:20 -0300

    Create BEE1026.cpp

---
+++ [2] Ad-hoc/BEE1026.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+unsigned int mofiz(unsigned int a, unsigned int b) {
+    return (~a & b) | (a & ~b);
+}
+
+int main() {
+    unsigned int x, y;
+    
+    while(cin >> x >> y) {
+        cout << mofiz(x, y) << endl;
+    }
+
+    return 0;
+}
