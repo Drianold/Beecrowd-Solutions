79b0f1d6c874f9e590ce5cde3f620c688e4a7fbf
Drianold <56211609+Drianold@users.noreply.github.com>
2021-12-01 17:34:57 -0300

    BEE1014

---
+++ [1] Iniciante/BEE1014.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <iomanip>
+
+using namespace std;
+
+int main() {
+    int x;
+    double y;
+
+    cin >> x >> y;
+
+    cout << fixed;
+    cout << setprecision(3);
+    cout << x/y << " km/l\n";
+}
