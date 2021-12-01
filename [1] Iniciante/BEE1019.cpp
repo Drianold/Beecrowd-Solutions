e0c4ca07b6a216d6d30a24c873384a3499bb7a7d
Drianold <56211609+Drianold@users.noreply.github.com>
2021-12-01 19:27:28 -0300

    BEE1019.cpp

---
+++ [1] Iniciante/BEE1019.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int n, h, m;
+
+    cin >> n;
+
+    h = int(n/3600);
+    n = n - h*3600;
+    m = int(n/60);
+    n = n - m*60;
+
+    cout << h << ":" << m << ":" << n;
+}
