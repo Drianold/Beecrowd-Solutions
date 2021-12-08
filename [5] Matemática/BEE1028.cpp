055f5bea7b0bbcb9496cefb4c861028b9e84d46f
Drianold <56211609+Drianold@users.noreply.github.com>
2021-12-07 22:57:20 -0300

    Update BEE1028.cpp
    Resolução utilizando o algoritmo de Euclides na forma recursiva

--- [5] Matemática/BEE1028.cpp
+++ [5] Matemática/BEE1028.cpp
@@ -1,18 +1,22 @@
 #include <iostream>
+#include <math.h>
 
 using namespace std;
 
+int tam_max_pilha_s(int a, int b) {
+    int resto = a % b;
+
+    if (resto == 0)
+        return b;
+    else
+        return tam_max_pilha_s(b, resto);
+}
+
 int tam_max_pilha(int f1, int f2) {
-    int i = f1, max;
-
-    if (i > f2)
-        i = f2;
-
-    while (f1 % i != 0 || f2 % i != 0) {
-        i--;
-    }
-
-    return i;
+    if (abs(f2) > abs(f1))
+        return tam_max_pilha_s(abs(f2), abs(f1));
+    else
+        return tam_max_pilha_s(abs(f1), abs(f2));
 }
 
 int main() {
