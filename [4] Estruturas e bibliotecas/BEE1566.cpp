cf1e7897db0fe27ce3b7bd557530d630ee901ae0
Drianold <56211609+Drianold@users.noreply.github.com>
2021-12-19 17:46:18 -0300

    Update BEE1566.cpp

--- [4] Estruturas e bibliotecas/BEE1566.cpp
+++ [4] Estruturas e bibliotecas/BEE1566.cpp
@@ -3,32 +3,29 @@
 using namespace std;
 
 int ordenar_e_imprimir(int n) {
-    int h, alturas[211];
-
-    for (int i = 0; i < 211; i++) {
-        alturas[i] = 0;
-    }
+    int min = 20, max = 230, h, spc = 0;
+    int alturas[max+1] = {0};
 
     for (int i = 0; i < n; i++) {
         cin >> h;
-        alturas[h-20] += 1;
+        alturas[h] += 1;
     }
 
-    for (int i = 0; i < 211; i++) {
+    for (int i = min; i <= max; i++) {
         for (int j = 0; j < alturas[i]; j++) {
-            cout << i + 20 << ' ';
+            if (spc++ == 0)
+                cout << i;
+            else
+                cout << ' ' << i;
         }
     }
     cout << endl;
-
-    // for (int i = 0; i < 211; i++) {
-    //     cout << alturas[i] << ' ';
-    // }
-    // cout << endl;
 }
 
 int main() {
     int nc, n;
+
+    ios::sync_with_stdio(false);
 
     cin >> nc;
 
