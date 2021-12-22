28da8b2aeba67fb730ee357cd048c407038e4bb3
Drianold <56211609+Drianold@users.noreply.github.com>
2021-12-21 23:30:18 -0300

    BEE1075

---
+++ [1] Iniciante/BEE1075.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int n, x;
+
+    cin >> n;
+
+    for (int i = 1; i <= 10000; i++) {
+        if (i % n == 2)
+            cout << i << endl;
+    }
+}
