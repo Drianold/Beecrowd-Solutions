05e1fbeadca6e5466338b4e3fb4a087f9ef1b7ab
Drianold <56211609+Drianold@users.noreply.github.com>
2022-01-10 19:55:36 -0300

    BEE1097

---
+++ [1] Iniciante/BEE1097.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    for (int i = 1; i <= 9; i += 2) {
+        for (int j = 7; j >= 5; j--) {
+            cout << "I=" << i << " J=" << j+(i-1) << endl;
+        }
+    }
+}
