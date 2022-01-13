a0ecd13466afc3a2aba9e39b935a3ef9c4125d98
Drianold <56211609+Drianold@users.noreply.github.com>
2022-01-13 09:58:12 -0300

    BEE1133

---
+++ [1] Iniciante/BEE1133.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int x, y, temp;
+
+    cin >> x >> y;
+
+    if (x > y) {
+        temp = x;
+        x = y;
+        y = temp;
+    }
+
+    for (int i = x+1; i < y; i++) {
+        if (i % 5 == 2 || i % 5 == 3)
+            cout << i << endl;
+    }
+}
