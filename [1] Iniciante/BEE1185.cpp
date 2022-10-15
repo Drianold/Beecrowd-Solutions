9a320f2e88d15040694e6aca1dcab98d93728dff
Drianold <56211609+Drianold@users.noreply.github.com>
2022-10-14 21:22:06 -0300

    BEE1185

---
+++ [1] Iniciante/BEE1185.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <iomanip>
+
+#define SIZE 12
+
+using namespace std;
+
+int main() {
+    double m[SIZE][SIZE];
+    char o;
+    double result = 0;
+
+    cin >> o;
+
+    for (int i = 0; i < SIZE; i++) {
+        for (int j = 0; j < SIZE; j++) {
+            cin >> m[i][j];
+        }
+    }
+
+    for (int i = 0; i < SIZE; i++) {
+        for (int j = 0; j < SIZE-i-1; j++) {
+            result += m[i][j];
+        }
+    }
+
+    if (o == 'M')
+        result /= (SIZE*SIZE-SIZE)/2.0;
+
+    cout << fixed;
+    cout << setprecision(1);
+    
+    cout << result << endl;
+}
